1eb87b407720fa136e5b07d5a61023cfd37d4874
Okoniewitz <37485333+Okoniewitz@users.noreply.github.com>
2024-01-25 16:01:09 +0100

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace WeaponMetaMerger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.File1 = new System.Windows.Forms.TextBox();
+            this.File2 = new System.Windows.Forms.TextBox();
+            this.File3 = new System.Windows.Forms.TextBox();
+            this.Start = new System.Windows.Forms.Button();
+            this.WorkSpace = new System.Windows.Forms.TextBox();
+            this.File1_Name = new System.Windows.Forms.TextBox();
+            this.File2_Name = new System.Windows.Forms.TextBox();
+            this.Edit1 = new System.Windows.Forms.TextBox();
+            this.Edit2 = new System.Windows.Forms.TextBox();
+            this.Save = new System.Windows.Forms.Button();
+            this.File1_Label = new System.Windows.Forms.Label();
+            this.File2_Label = new System.Windows.Forms.Label();
+            this.Merge = new System.Windows.Forms.Button();
+            this.File4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // File1
+            // 
+            this.File1.Location = new System.Drawing.Point(12, 83);
+            this.File1.Multiline = true;
+            this.File1.Name = "File1";
+            this.File1.ReadOnly = true;
+            this.File1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.File1.Size = new System.Drawing.Size(462, 574);
+            this.File1.TabIndex = 0;
+            this.File1.WordWrap = false;
+            // 
+            // File2
+            // 
+            this.File2.Location = new System.Drawing.Point(480, 83);
+            this.File2.Multiline = true;
+            this.File2.Name = "File2";
+            this.File2.ReadOnly = true;
+            this.File2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.File2.Size = new System.Drawing.Size(469, 574);
+            this.File2.TabIndex = 1;
+            this.File2.WordWrap = false;
+            // 
+            // File3
+            // 
+            this.File3.Location = new System.Drawing.Point(955, 83);
+            this.File3.Multiline = true;
+            this.File3.Name = "File3";
+            this.File3.ReadOnly = true;
+            this.File3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.File3.Size = new System.Drawing.Size(453, 574);
+            this.File3.TabIndex = 2;
+            this.File3.WordWrap = false;
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(1443, 81);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 3;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // WorkSpace
+            // 
+            this.WorkSpace.Location = new System.Drawing.Point(1455, 123);
+            this.WorkSpace.Name = "WorkSpace";
+            this.WorkSpace.Size = new System.Drawing.Size(100, 20);
+            this.WorkSpace.TabIndex = 4;
+            this.WorkSpace.Text = "F:\\SteamLibrary\\steamapps\\common\\Grand Theft Auto V\\scripts\\Pickups & Real shit\\u" +
+    "pdaterpf\\common\\data\\ai";
+            // 
+            // File1_Name
+            // 
+            this.File1_Name.Location = new System.Drawing.Point(1455, 172);
+            this.File1_Name.Name = "File1_Name";
+            this.File1_Name.Size = new System.Drawing.Size(100, 20);
+            this.File1_Name.TabIndex = 5;
+            this.File1_Name.Text = "pick";
+            // 
+            // File2_Name
+            // 
+            this.File2_Name.Location = new System.Drawing.Point(1455, 198);
+            this.File2_Name.Name = "File2_Name";
+            this.File2_Name.Size = new System.Drawing.Size(100, 20);
+            this.File2_Name.TabIndex = 6;
+            this.File2_Name.Text = "ripp";
+            // 
+            // Edit1
+            // 
+            this.Edit1.Location = new System.Drawing.Point(1455, 350);
+            this.Edit1.Name = "Edit1";
+            this.Edit1.Size = new System.Drawing.Size(100, 20);
+            this.Edit1.TabIndex = 7;
+            this.Edit1.Text = "<Fx>";
+            // 
+            // Edit2
+            // 
+            this.Edit2.Location = new System.Drawing.Point(1455, 376);
+            this.Edit2.Name = "Edit2";
+            this.Edit2.Size = new System.Drawing.Size(100, 20);
+            this.Edit2.TabIndex = 8;
+            this.Edit2.Text = "<AttachPoints>";
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(1443, 463);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 11;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // File1_Label
+            // 
+            this.File1_Label.AutoSize = true;
+            this.File1_Label.Location = new System.Drawing.Point(59, 9);
+            this.File1_Label.Name = "File1_Label";
+            this.File1_Label.Size = new System.Drawing.Size(35, 13);
+            this.File1_Label.TabIndex = 12;
+            this.File1_Label.Text = "label1";
+            // 
+            // File2_Label
+            // 
+            this.File2_Label.AutoSize = true;
+            this.File2_Label.Location = new System.Drawing.Point(59, 34);
+            this.File2_Label.Name = "File2_Label";
+            this.File2_Label.Size = new System.Drawing.Size(35, 13);
+            this.File2_Label.TabIndex = 13;
+            this.File2_Label.Text = "label1";
+            // 
+            // Merge
+            // 
+            this.Merge.Location = new System.Drawing.Point(1443, 308);
+            this.Merge.Name = "Merge";
+            this.Merge.Size = new System.Drawing.Size(75, 23);
+            this.Merge.TabIndex = 14;
+            this.Merge.Text = "Merge";
+            this.Merge.UseVisualStyleBackColor = true;
+            this.Merge.Click += new System.EventHandler(this.Merge_Click);
+            // 
+            // File4
+            // 
+            this.File4.Location = new System.Drawing.Point(27, 642);
+            this.File4.Multiline = true;
+            this.File4.Name = "File4";
+            this.File4.ReadOnly = true;
+            this.File4.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.File4.Size = new System.Drawing.Size(48, 63);
+            this.File4.TabIndex = 15;
+            this.File4.Visible = false;
+            this.File4.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1581, 704);
+            this.Controls.Add(this.File4);
+            this.Controls.Add(this.Merge);
+            this.Controls.Add(this.File2_Label);
+            this.Controls.Add(this.File1_Label);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.Edit2);
+            this.Controls.Add(this.Edit1);
+            this.Controls.Add(this.File2_Name);
+            this.Controls.Add(this.File1_Name);
+            this.Controls.Add(this.WorkSpace);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.File3);
+            this.Controls.Add(this.File2);
+            this.Controls.Add(this.File1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox File1;
+        private System.Windows.Forms.TextBox File2;
+        private System.Windows.Forms.TextBox File3;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.TextBox WorkSpace;
+        private System.Windows.Forms.TextBox File1_Name;
+        private System.Windows.Forms.TextBox File2_Name;
+        private System.Windows.Forms.TextBox Edit1;
+        private System.Windows.Forms.TextBox Edit2;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Label File1_Label;
+        private System.Windows.Forms.Label File2_Label;
+        private System.Windows.Forms.Button Merge;
+        private System.Windows.Forms.TextBox File4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WeaponMetaMerger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string[] TextFile = new string[2] { "", "" };
+        private void Start_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] Files = new string[2] { File1_Name.Text, File2_Name.Text };
+                string[] files = Directory.GetFiles(WorkSpace.Text);
+                TextFile[0] = FindFile(0, files, Files);
+                TextFile[1] = FindFile(1, files, Files);
+                File1.Text = File.ReadAllText(TextFile[0]);
+                File2.Text = File.ReadAllText(TextFile[1]);
+                File1_Label.Text = TextFile[0];
+                File2_Label.Text = TextFile[1];
+            } catch(Exception exc) { MessageBox.Show(exc.Message,"Błąd"); }
+        }
+
+        private string FindFile(int index, string[] str, string[] neededStr)
+        {
+            foreach (string file in str)
+            {
+                if (file.Contains(neededStr[index]))
+                    return file;
+            }
+            return "";
+        }
+        private int[] FindLines(string[] File, string From, string To)
+        {
+            int FromIndex=0, ToIndex=0;
+            foreach (string line in File)
+            {
+                if (line.Contains(From) && FromIndex == 0)
+                    FromIndex = Array.IndexOf(File, line) + 1;
+                if (line.Contains(To) && FromIndex != 0)
+                    ToIndex = Array.IndexOf(File, line) + 1;
+            }
+            return new int[2] { FromIndex, ToIndex };
+        }
+
+        private void Merge_Click(object sender, EventArgs e)
+        {
+            string From = Edit1.Text;
+            string To = "</"+Edit1.Text.Substring(1,Edit1.Text.Length-1);
+            string EditText="";
+            int[]Lines = FindLines(File2.Lines, From, To);
+            List<string> EditLines = new List<string>();
+            foreach(string line in File2.Lines)
+            {
+                 if(Array.IndexOf(File2.Lines, line)>= Lines[0]- 1 && Array.IndexOf(File2.Lines, line)<= Lines[1]- 1)
+                {
+                    EditLines.Add(line);
+                }
+            }
+            string TextBefore = File1.Text.Substring(0, File1.Text.IndexOf(From));
+            string TextAfter = File1.Text.Substring(File1.Text.IndexOf(To)+To.Length, File1.Text.Length - File1.Text.IndexOf(To)-To.Length);
+            foreach(string line in EditLines)
+            {
+                EditText += line+ Environment.NewLine;
+            }
+            File4.Text = TextBefore +EditText+ TextAfter;
+            EditLines.Clear();
+
+            From = Edit2.Text;
+            To = "</" + Edit2.Text.Substring(1, Edit2.Text.Length - 1);
+            EditText = "";
+            Lines = FindLines(File2.Lines, From, To);
+            foreach (string line in File2.Lines)
+            {
+                if (Array.IndexOf(File2.Lines, line) >= Lines[0] - 1 && Array.IndexOf(File2.Lines, line) <= Lines[1] - 1)
+                {
+                    EditLines.Add(line);
+                }
+            }
+            TextBefore = File4.Text.Substring(0, File4.Text.IndexOf(From));
+            TextAfter = File4.Text.Substring(File4.Text.IndexOf(To) + To.Length, File4.Text.Length - File4.Text.IndexOf(To) - To.Length);
+            foreach (string line in EditLines)
+            {
+                EditText += line + Environment.NewLine;
+            }
+            File3.Text = TextBefore + EditText + TextAfter;
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            try
+            { 
+            File.WriteAllText(TextFile[0].Substring(0,TextFile[0].IndexOf("pickups"))+".meta", File3.Text);
+                File.Delete(TextFile[0]);
+                File.Delete(TextFile[1]);
+            }
+            catch (Exception exc) { MessageBox.Show(exc.Message, "Błąd"); }
+        }
+    }
+}
